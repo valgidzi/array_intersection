326ad716bc4da617226306c97e0ab09a7b040a27
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-09 23:32:12 -0800

    Rewrite method using hash table

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -4,18 +4,27 @@ def intersection(array1, array2)
 
   return array if array1.nil? || array2.nil?
 
-  array1.length.times do |m|
-    array2.length.times do |n|
-      if array1[m] == array2[n]
-        array << array1[m]
-      end
+  if array1.length > array2.length
+    larger = array1
+    smaller = array2
+  else
+    larger = array2
+    smaller = array1
+  end
+
+  hash = Hash.new()
+  smaller.each do |sm_num|
+    hash[sm_num] = 1
+  end
+
+  larger.each do |lg_num|
+    if hash.include? lg_num
+      array << lg_num
     end
   end
 
   return array
 end
 
-# Time complexity: linear
-#  Given one array of length m and another of length n, the time complexity is O(mn).
-# Space complexity: linear
-#  The space complexity is also O(mn). The size of the returned array depnds on the length of the arrays being compared.
+# Time complexity: O(n + m)
+# Space complexity: O(n)
